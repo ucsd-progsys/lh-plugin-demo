bff39072729904f1538db9b6966773901a5de017
Ranjit Jhala <jhala@cs.ucsd.edu>
2020-07-29 11:24:57 -0700

    update emacs.png

--- src/Demo/Client.hs
+++ src/Demo/Client.hs
@@ -2,7 +2,8 @@ module Demo.Client where
 
 import Demo.Lib
 
-bump n = if n > 0 then incr n else 0
+bump :: Int -> Int
+bump n = incr n
 
 {-@ ignore thisIsUnsafe @-} -- delete this to see an error
 thisIsUnsafe :: Int -> Int
