c96a491d5def0dad6860a9c0f9c7be9ae7d67402
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-08-06 10:42:43 -0700

    update stack.yaml

--- src/Demo/Client.hs
+++ src/Demo/Client.hs
@@ -1,6 +1,25 @@
+{-@ LIQUID "--reflection"     @-}
+{-@ LIQUID "--ple"            @-}
+{-@ LIQUID "--no-termination" @-}
+
 module Demo.Client where
+
+import Language.Haskell.Liquid.Equational
 
 import Demo.Lib
 
 bump :: Int -> Int
 bump n = if n > 0 then incr n else 0
+
+data Simple = A | B | C
+
+{-@ reflect test @-}
+test :: Simple -> Maybe Bool
+test A = Just True
+test _ = Nothing
+
+{-@ testProof :: {test A == Just True} @-}
+testProof :: Proof
+testProof =
+  test A ==. Just True
+  *** QED
