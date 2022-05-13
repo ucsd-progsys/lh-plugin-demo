175dda68aa29a9b35d9789a38ff5a4055add8c25
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-05-13 09:16:43 -0700

    tudor demo

--- src/Demo/Lib.hs
+++ src/Demo/Lib.hs
@@ -1,7 +1,15 @@
 module Demo.Lib where
 
+import Data.Set
+
 {-@ type Pos = {v:Int | 0 < v} @-}
 
 {-@ incr :: Pos -> Pos @-}
 incr :: Int -> Int
 incr x = x + 1
+
+
+{-@ measure elts @-}
+elts        :: (Ord a) => [a] -> Set a
+elts []     = empty
+elts (x:xs) = singleton x `union` elts xs
